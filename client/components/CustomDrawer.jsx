3f87bcae94b50d40f24308a8260a0818d7851c84
Nikhil Simon Toppo <nikhilsimontoppo@gmail.com>
2023-09-29 03:12:13 +0530

    logos added in sidebar

---
+++ client/components/CustomDrawer.jsx
@@ -0,0 +1,19 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+
+const CustomDrawer = props => {
+  return (
+    <DrawerContentScrollView
+      {...props}
+      style={{backgroundColor: '#DDFAFF'}}
+      contentContainerStyle={{backgroundColor: '#DDFAFF'}}>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
+
+export default CustomDrawer;
