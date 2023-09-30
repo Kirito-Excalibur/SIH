635ae3a8f872489191119b0a1e4ae4f153053869
Nikhil Simon Toppo <nikhilsimontoppo@gmail.com>
2023-09-30 19:10:53 +0530

    Settings almost yet

--- client/pages/Settings.jsx
+++ client/pages/Settings.jsx
@@ -1,11 +1,12 @@
 import {View, StyleSheet, Switch} from 'react-native';
 import React, {useState} from 'react';
-import {Divider, Text} from 'react-native-paper';
+import {Divider, Text, RadioButton} from 'react-native-paper';
 import TopBar from '../components/TopBar';
-
+import Slider from '@react-native-community/slider';
 const Settings = ({navigation}) => {
   const [isSwitchOn, setIsSwitchOn] = useState(false);
-
+  const [checked, setChecked] = useState('first');
+  const [font, setFont] = useState(14);
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
 
   return (
@@ -17,12 +18,63 @@ const Settings = ({navigation}) => {
           alignItems: 'center',
           justifyContent: 'center',
         }}>
-        <View style={styles.inner_container}>
+        <View style={styles.language}>
           <Text style={styles.title}>Language</Text>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+
+              width: 100,
+            }}>
+            <Text style={{fontSize: 17}}>English</Text>
+            <RadioButton
+              value="first"
+              color="black"
+              status={checked === 'first' ? 'checked' : 'unchecked'}
+              onPress={() => setChecked('first')}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+
+              width: 100,
+            }}>
+            <Text style={{fontSize: 17}}>Hindi</Text>
+            <RadioButton
+              value="second"
+              color="black"
+              status={checked === 'second' ? 'checked' : 'unchecked'}
+              onPress={() => setChecked('second')}
+            />
+          </View>
         </View>
         <Divider />
-        <View style={styles.inner_container}>
+        <View style={styles.font_size}>
           <Text style={styles.title}>Font Size</Text>
+          <View
+            style={{
+              // borderWidth: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{fontSize: font}}>Anthracite</Text>
+          </View>
+
+          <Slider
+            style={{width: 350, height: 40}}
+            minimumValue={9}
+            maximumValue={30}
+            value={font}
+            onValueChange={e => setFont(e)}
+            minimumTrackTintColor="#FFFFFF"
+            maximumTrackTintColor="#000000"
+          />
         </View>
         <Divider />
         <View style={styles.notification}>
@@ -31,7 +83,7 @@ const Settings = ({navigation}) => {
         </View>
         <Divider />
         <View style={styles.inner_container}>
-          <Text style={styles.title}> Help</Text>
+          <Text style={styles.title}>Help</Text>
         </View>
         <Divider />
         <View style={styles.inner_container}>
@@ -46,11 +98,23 @@ const Settings = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
   },
   inner_container: {
     paddingVertical: 5,
     paddingHorizontal: 30,
   },
+  language: {
+    paddingVertical: 5,
+    paddingHorizontal: 30,
+    gap: 10,
+  },
+  font_size: {
+    paddingVertical: 5,
+    paddingHorizontal: 30,
+    gap: 10,
+  },
+
   notification: {
     paddingVertical: 5,
     paddingHorizontal: 30,
